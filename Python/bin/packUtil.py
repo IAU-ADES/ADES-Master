1990ae5dd4c0140ae82f313b9a452a2f460b3424
Steve Chesley <steve.chesley@jpl.nasa.gov>
2024-04-23 12:41:38 -0700

    Deleted British Pound symbol '£' in anticipation of MPC removing it from their ProgramCodes.txt soon.

--- Python/bin/packUtil.py
+++ Python/bin/packUtil.py
@@ -50,7 +50,8 @@ validCodes = "A PeCBTMcEOHNnRrSsVvXx"+"0"  # 0 is special for header lines
 validNotes = ' AaBbcDdEFfGgGgHhIiJKkMmNOoPpRrSsTtUuVWwYyCQX2345vzjeL16789'
 # Triple quotes to handle the double quotation mark in the string and not munge the index of characters.
 # New value as of 19 Mar 2024. Downloaded from https://www.minorplanetcenter.net/iau/lists/ProgramCodes.txt
-programCodesArray = R"""0123456789!"#$%&'()*+,-./[\]^_`{|}~:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz£"""
+# But deleted British Pound symbol '£' in anticipation of MPC removing it from their ProgramCodes.txt soon.
+programCodesArray = R"""0123456789!"#$%&'()*+,-./[\]^_`{|}~:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"""
 validProgramCodes = ' ' + programCodesArray
 
 programCodeSites = \
