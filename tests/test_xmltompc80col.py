855dc55001646ab2221ae1fb07ba721ee4f0daf6
Peter Williams <peter@newton.cx>
2026-06-11 15:44:33 -0400

    Streamline test_xmltompc80col.py

--- tests/test_xmltompc80col.py
+++ tests/test_xmltompc80col.py
@@ -10,77 +10,25 @@ from tempfile import NamedTemporaryFile
 import pytest
 
 
-def test_trksub_submission():
-    """Test trksub submission"""
-    infile = "input/trksub_sub.xml"
-    outfile = "output/trksub_sub.obs"
-    # Remove outfile if there
-    if os.path.exists(outfile):
-        os.remove(outfile)
-    subprocess.run(
-        "xmltompc80col.py " + infile + " " + outfile,
-        shell=True,
-        check=False,
-    )
-    assert os.path.exists(outfile) and os.stat(outfile).st_size != 0
+def _test_doesnt_crash(inpath):
+    with NamedTemporaryFile(mode="w+t", suffix=".obs") as f_output_temp:
+        subprocess.run(
+            ["xmltompc80col.py", inpath, f_output_temp.name],
+            shell=False,
+            check=True,
+        )
+
+        assert os.path.exists(f_output_temp.name)
+        assert os.stat(f_output_temp.name).st_size != 0
 
 
-def test_trksub_submission_newregex():
-    """After changing the regex in xmltompc80col.py, test trksub submission"""
-    infile = "input/gb_trksub.xml"
-    outfile = "output/gb_trksub.obs"
-    # Remove outfile if there
-    if os.path.exists(outfile):
-        os.remove(outfile)
-    subprocess.run(
-        "xmltompc80col.py " + infile + " " + outfile,
-        shell=True,
-        check=False,
-    )
-    assert os.path.exists(outfile) and os.stat(outfile).st_size != 0
-
-
-def test_trksub_8char():
-    """Converting the file even if the trksub is 8-char long"""
-    infile = "input/trksub_8char.xml"
-    outfile = "output/trksub_8char.obs"
-    # Remove outfile if there
-    if os.path.exists(outfile):
-        os.remove(outfile)
-    subprocess.run(
-        "xmltompc80col.py " + infile + " " + outfile,
-        shell=True,
-        check=False,
-    )
-    assert os.path.exists(outfile) and os.stat(outfile).st_size != 0
-
-
-def test_provid_notrksub():
-    """Converting the file even if trksub=provid """
-    infile = "input/provid_no_trksub.xml"
-    outfile = "output/provid_no_trksub.obs"
-    # Remove outfile if there
-    if os.path.exists(outfile):
-        os.remove(outfile)
-    subprocess.run(
-        "xmltompc80col.py " + infile + " " + outfile,
-        shell=True,
-        check=False,
-    )
-    assert os.path.exists(outfile) and os.stat(outfile).st_size != 0
-
-
-def test_band_conversions():
-    """Testing some edge cases mapping ADES to obs80 band designations"""
-    infile = "input/band_conversions.xml"
-    expfile = "expected/band_conversions.obs"
-
-    with open(expfile) as f_expected:
+def _test_output_is_expected(inpath, exppath):
+    with open(exppath) as f_expected:
         expected = f_expected.read()
 
     with NamedTemporaryFile(mode="w+t", suffix=".obs") as f_output_temp:
         subprocess.run(
-            ["xmltompc80col.py", infile, f_output_temp.name],
+            ["xmltompc80col.py", inpath, f_output_temp.name],
             shell=False,
             check=True,
         )
@@ -90,18 +38,32 @@ def test_band_conversions():
     assert observed == expected
 
 
+def test_trksub_submission():
+    """Test trksub submission"""
+    _test_doesnt_crash("input/trksub_sub.xml")
+
+
+def test_trksub_submission_newregex():
+    """After changing the regex in xmltompc80col.py, test trksub submission"""
+    _test_doesnt_crash("input/gb_trksub.xml")
+
+
+def test_trksub_8char():
+    """Converting the file even if the trksub is 8-char long"""
+    _test_doesnt_crash("input/trksub_8char.xml")
+
+
+def test_provid_notrksub():
+    """Converting the file even if trksub=provid """
+    _test_doesnt_crash("input/provid_no_trksub.xml")
+
+
+def test_band_conversions():
+    """Testing some edge cases mapping ADES to obs80 band designations"""
+    _test_output_is_expected("input/band_conversions.xml", "expected/band_conversions.obs")
+
+
 @pytest.mark.xfail
 def test_trksub_9char():
     """ The code should stop with an error for any trksubs longer than 8 chars"""
-    infile = "input/trksub_9chars.xml"
-    outfile = "output/trksub_chars.obs"
-    # Remove outfile if there
-    if os.path.exists(outfile):
-        os.remove(outfile)
-    result = subprocess.run(
-        "xmltompc80col.py " + infile + " " + outfile,
-        shell=True,
-        check=True,
-    )
-
-    assert result.returncode == 0
+    _test_doesnt_crash("input/trksub_9chars.xml")
