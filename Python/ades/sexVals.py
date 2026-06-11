3fea0d88a3d3147688d72d528c973a775c32c5dd
Peter Williams <peter@newton.cx>
2026-06-11 13:55:03 -0400

    Correct RA-to-sexagesimal code to not produce `60.0` seconds
    This is a classic mistake: you can't wait until the end of your
    breakdown to round the seconds.

--- Python/ades/sexVals.py
+++ Python/ades/sexVals.py
@@ -13,11 +13,8 @@ from __future__ import unicode_literals
 
 import sys
 import re
-import io
 import math
 from datetime import datetime, timedelta
-
-#import adesutility
 
 
 def errorSexVal(msg, l):
@@ -70,6 +67,7 @@ _countIntegerSeconds = 0
 _countMinutesHundredths = 0
 _countMinutesTenths = 0
 _countIntegerMinutes = 0
+
 def _actionNormal(m):
    """ action for HH MM SS.ss..."""
    global _countNormal
@@ -216,6 +214,7 @@ _countDate100000 = 0
 _countDate1000000 = 0
 _countDateBig = 0
 _countDateSmall = 0
+
 def twoDigit(t):
    ss = "{0:.0f}".format(t+100.0)[1:]
    return ss
@@ -403,33 +402,81 @@ def sexRaToDecRa( sexRa ):
    #print ("rdict['raSexagesimal'] = ", hours, minutes, seconds, prec, value, digits, rdict['ra'])
    return (raDec, prec)
 
-def decRaToSexRa( decRa, prec ):
-   """ converts decimal degress Ra to sexagesimal Ra
-       Inputs: 
-         decRa: string decimal degrees RA 
-         prec: precison value
-       Return Value: 
-         sexRa:  string sexagesimal RA value in hms
+def decRaToSexRa(decRa, prec):
    """
-   rev = float(decRa)/secToDegrees
+   Converts decimal degrees RA to sexagesimal Ra
 
-   revDeg = int(rev/3600.0);
-   rev = rev - revDeg * 3600.0
-   if prec == 0.6:
-      revstr = '{0:.0f}'.format(100+revDeg)[1:] + ' ' + '{0:.2f}'.format(100.0 + rev/60.0)[1:]
+   Inputs: 
+      decRa: decimal degrees RA; type is anything that can be converted to a float
+      prec: float, precison value (see below)
+
+   Return Value: 
+      sexRa: string, sexagesimal RA value in hms
+
+   The returned string will be at least twelve characters long, with padding
+   spaces added at the end if needed. Different values of *prec* result
+   in different outputs as follows:
+
+   - 60   => `HH MM       `
+   - 6    => `HH MM.M     `
+   - 0.6  => `HH MM.MM    `
+   - 1    => `HH MM SS    `
+   - 0.1  => `HH MM SS.S  `
+   - 0.01 => `HH MM SS.SS `
+   - 1e-5 => `HH MM SS.SSSSS`, etc.
+   """
+
+   tot_seconds = float(decRa) / secToDegrees
+
+   if tot_seconds < 0 or tot_seconds >= 86400.:
+      raise RuntimeError("illegal decRaToSexRa input {0!r}: out of numerical bounds".format(decRa))
+
+   if prec == 60:
+      minutes_mode = True
+      places = 0
    elif prec == 6:
-      revstr = '{0:.0f}'.format(100+revDeg)[1:] + ' ' + '{0:.1f}'.format(100.0 + rev/60.0)[1:]
-   elif prec == 60:
-      revstr = '{0:.0f}'.format(100+revDeg)[1:] + ' ' + '{0:.0f}'.format(100.0 + rev/60.0)[1:]
+      minutes_mode = True
+      places = 1
+   elif prec == 0.6:
+      minutes_mode = True
+      places = 2
    else:
-      revMin = int(rev/60.0)
-      rev = rev - revMin * 60.0
-      pdig = -int(math.log10(prec))
-      fmt = '{0:.' + repr(pdig) + 'f}'
-      #print (pdig, fmt)
-      revstr = '{0:.0f}'.format(100+revDeg)[1:] + ' ' + '{0:.0f}'.format(100+revMin)[1:] + ' ' + fmt.format(100.0 + rev)[1:]
+      minutes_mode = False
+      places = -int(math.log10(prec))
 
-   return "{0:12s}".format(revstr)  # get length right
+   if places < 1:
+      width = places + 2  # no decimal point
+   else:
+      width = places + 3  # yes decimal point
+
+   # We need to round the rightmost fractional item *before* breaking down into
+   # H/M/S to avoid `60.0` situations. But the "native" unit of this item
+   # depends on whether we're in "minutes mode" or not. Eithe way, we also need
+   # to check for rounding up to the wraparound value.
+
+   if minutes_mode:
+      tot_minutes = tot_seconds / 60
+      tot_minutes = round(tot_minutes, places)
+      
+      if tot_minutes >= 1440.:
+         tot_minutes -= 1440  # this could happen if we round from 23:59:59.9 to 24:00:00
+
+      hours = int(tot_minutes // 60)
+      minutes = tot_minutes - hours * 60
+      result = "%02d %0*.*f" % (hours, width, places, minutes)
+   else:
+      tot_seconds = round(tot_seconds, places)
+
+      if tot_seconds >= 86400.:
+         tot_seconds -= 86400
+
+      hours = int(tot_seconds // 3600)
+      seconds = tot_seconds - hours * 3600
+      minutes = int(seconds // 60)
+      seconds = seconds - minutes * 60
+      result = "%02d %02d %0*.*f" % (hours, minutes, width, places, seconds)
+
+   return "{0:12s}".format(result)  # get length right
    
 def checkRa(rdict):
    """ checks that rdict['raSexagesimal'] is valid and adds 
